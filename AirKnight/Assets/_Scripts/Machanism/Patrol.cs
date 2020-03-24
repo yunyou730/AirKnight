061724bc9b791771052cae85bea0b0324258c484
ayy730 <502765299@qq.com>
2020-03-24 09:48:10 +0800

    +trying to add float patrol platform , WIP

---
+++ AirKnight/Assets/_Scripts/Machanism/Patrol.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ak
+{
+    public class Patrol : MonoBehaviour
+    {
+        public Transform[] points = null;
+        private int curPointIndex = 0;
+
+        void Start()
+        {
+            SetPositionTo(0);
+            SetCurrentPointIndex(curPointIndex);
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void SetPositionTo(int pointIndex)
+        {
+            if (pointIndex < 0 || pointIndex >= points.Length)
+            {
+                return;
+            }
+            Transform point = points[pointIndex];
+            // @miao @todo
+        }
+
+        private void ApproachTo(int pointIndex)
+        {
+            if (pointIndex < 0 || pointIndex >= points.Length)
+            {
+                return;
+            }
+            Transform point = points[pointIndex];
+            // @miao @todo
+        }
+
+
+        private int GetPrevPointIndex(int pointIndex)
+        {
+            int prev = pointIndex - 1;
+            prev = prev < 0 ? points.Length - 1 : prev;
+            return prev;
+        }
+
+
+        private int GetNextPointIndex(int pointIndex)
+        {
+            int next = pointIndex + 1;
+            next = next == points.Length ? 0 : next;
+            return next;
+        }
+
+        private void SetCurrentPointIndex(int pointIndex)
+        {
+            curPointIndex = pointIndex;
+        }
+    }
+
+}
---
+++ AirKnight/Assets/_Scripts/Util/Actions/MoveTo.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ak
+{
+    public class MoveTo
+    {
+        Vector3 from, to;
+        float duration = 0;
+
+        float elapsedTime = 0;
+        bool isStarted = 0;
+
+        public delegate void FinishDelegate();
+        FinishDelegate callback = null;
+
+
+        public void Prepare(Vector3 from,Vector3 to,float duration)
+        {
+            this.from = from;
+            this.to = to;
+            this.duration = duration;
+        }
+
+        public void Run(FinishDelegate callback = null)
+        {
+            elapsedTime = 0;
+            this.callback = callback;
+        }
+
+        public void Update(float deltaTime)
+        {
+            float pct = elapsedTime / duration;
+            float displayPercent = GetDisplayPercentWithTimePercent(pct);
+
+
+
+            elapsedTime += deltaTime;
+        }
+
+
+        private float GetDisplayPercentWithTimePercent(float timePercent)
+        {
+            return timePercent;
+        }
+    }
+}
