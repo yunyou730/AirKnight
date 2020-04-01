49e737eafd5f1f8af6066bcb05c6f4af044172b7
ayy730 <502765299@qq.com>
2020-04-02 01:22:57 +0800

    + 给 空洞骑士 资源 增加 animator , 使用 animator + blend tree 实现 走路 细节

---
+++ AirKnight/Assets/_testcase2/input/InputButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ff
+{
+    public class InputButton
+    {
+        public bool isPress = false;
+        public bool isHold = false;
+        public bool isRelease = false;
+
+        string m_buttonKey;
+
+        public InputButton(string buttonKey)
+        {
+            m_buttonKey = buttonKey;
+        }
+
+        public void Update()
+        {
+            isPress = Input.GetButtonDown(m_buttonKey);
+            isHold = Input.GetButton(m_buttonKey);
+            isRelease = Input.GetButtonUp(m_buttonKey);
+        }
+    }
+
+}
