8472e5748b8849d304d90c2ce58f790167b3ff9f
ayy730 <502765299@qq.com>
2020-04-03 02:54:25 +0800

    +增加跳跃状态

--- AirKnight/Assets/_testcase2/actors/Aries/AriesController.cs
+++ AirKnight/Assets/_testcase2/actors/Aries/AriesController.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 namespace ff
 {
-    [RequireComponent(typeof(AriesAnimBridge),typeof(Animator))]
+    [RequireComponent(typeof(AriesAnimBridge),typeof(Animator), typeof(ff.EnvironmentDetector))]
     public class AriesController : MonoBehaviour
     {
         Animator m_animator = null;
         AriesAnimBridge m_animBridge = null;
-
-        public bool isMidAir = false;
         
-
         private ff.InputAxe m_horizontalAxe = null;
         private ff.InputButton m_jumpButton = null;
+
+        private ff.EnvironmentDetector m_envDetector = null;
 
         private void Awake()
         {
             m_animator = GetComponent<Animator>();
-            m_animBridge = GetComponent<AriesAnimBridge>();
+            m_animBridge = GetComponent<ff.AriesAnimBridge>();
+            m_envDetector = GetComponent<ff.EnvironmentDetector>();
 
             m_horizontalAxe = new ff.InputAxe("Horizontal");
             m_jumpButton = new ff.InputButton("Jump");
@@ -34,15 +34,38 @@ namespace ff
         {
             float dt = Time.deltaTime;
             CollectInput(dt);
+            UpdateStateFlag();
+        }
+
+        private void FixedUpdate()
+        {
+            
         }
 
         private void CollectInput(float dt)
         {
+            // horizon move
             m_horizontalAxe.Update(dt);
             m_animator.SetFloat(m_animBridge.horizonAxeHoldTime, m_horizontalAxe.m_holdDuration);
             m_animator.SetBool(m_animBridge.isHorizonAxeHold,m_horizontalAxe.IsHolding());
 
+            // jump
             m_jumpButton.Update(dt);
+            if (m_jumpButton.IsPress() && m_envDetector.isOnGround)
+            {
+
+                m_envDetector.PerformJump();
+            }
+        }
+
+
+        private void UpdateStateFlag()
+        {
+            // air flag
+            m_animator.SetBool(m_animBridge.isMidAir, !m_envDetector.isOnGround);
+
+            // vertical speed
+            m_animator.SetFloat(m_animBridge.airSpeed, m_envDetector.GetVerticalSpeed());
         }
     }
 
---
+++ AirKnight/Assets/_testcase2/common/EnvironmentDetector.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace ff
+{
+    // 为了调试 动画， 临时增加 环境检测脚本EnvironmentCheck
+    // 将来 可能随时替换成其他 物理检测手段
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class EnvironmentDetector : MonoBehaviour
+    {
+        // ground checker
+        public bool isOnGround = false;
+        public LayerMask groundLayer;
+        public float groundCheckerLength = 0.3f;
+        public Vector2 groundCheckerBeginOffset;
+        private Vector2 groundCheckerFrom;
+        private Color groundCheckerLineColor = Color.green;
+
+        // rigid body
+        Rigidbody2D m_rigidBody = null;
+
+        // jump
+        public float m_jumpForce = 10;
+
+        private void Awake()
+        {
+            m_rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        void Start()
+        {
+            
+        }
+        
+        void Update()
+        {
+            
+        }
+
+        void FixedUpdate()
+        {
+            CheckGround();
+        }
+        
+        void CheckGround()
+        {
+            isOnGround = false;
+
+            groundCheckerFrom.x = transform.position.x + groundCheckerBeginOffset.x;
+            groundCheckerFrom.y = transform.position.y + groundCheckerBeginOffset.y;
+            RaycastHit2D hit = Physics2D.Raycast(groundCheckerFrom, -Vector2.up, groundCheckerLength, groundLayer);
+
+            groundCheckerLineColor = Color.green;
+            if (hit.collider != null)
+            {
+                groundCheckerLineColor = Color.red;
+                isOnGround = true;
+            }
+            Debug.DrawRay(groundCheckerFrom, -Vector2.up * groundCheckerLength, groundCheckerLineColor);
+        }
+
+
+        public float GetVerticalSpeed()
+        {
+            return m_rigidBody.velocity.y;
+        }
+
+        public void PerformJump()
+        {
+            m_rigidBody.AddForce(new Vector2(0,m_jumpForce), ForceMode2D.Force);
+        }
+        
+    }
+}
