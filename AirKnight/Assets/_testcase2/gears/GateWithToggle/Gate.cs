0b7f77c5c21331bc573df545a48636c0ba15ebe1
ayy730 <502765299@qq.com>
2020-04-24 02:02:43 +0800

    +去掉不用的资源，增加新资源，增加简单的开关门

---
+++ AirKnight/Assets/_testcase2/gears/GateWithToggle/Gate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    Renderer  m_renderer = null;
+    Collider2D  m_collider = null;
+
+    bool    m_hasTurnOn = false;
+
+    List<GateSwitcher>  m_switchList = new List<GateSwitcher>();
+    void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+        m_collider = GetComponent<BoxCollider2D>();
+    }
+
+    public void TurnOn()
+    {
+        if(m_hasTurnOn)
+        {
+            return;
+        }
+        m_hasTurnOn = true;
+        TurnOnGate();
+        PlayOpenAnim();
+    }
+
+
+    private void TurnOnGate()
+    {
+        m_collider.enabled = false;
+        foreach(GateSwitcher switcher in m_switchList)
+        {
+            switcher.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlayOpenAnim()
+    {
+        m_renderer.enabled = false;
+    }  
+
+
+    public void  RegisterSwitch(GateSwitcher switcher)
+    {
+        m_switchList.Add(switcher);
+    }  
+}
---
+++ AirKnight/Assets/_testcase2/gears/GateWithToggle/GateSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSwitcher : MonoBehaviour
+{
+    Gate    m_gate = null;
+
+    void Awake()
+    {
+        m_gate = transform.parent.GetComponent<Gate>();
+        m_gate.RegisterSwitch(this);
+    }
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        m_gate.TurnOn();
+    }
+}
