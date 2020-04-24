e548b4702b02387a0785492a04bc3d9950a75674
ayy730 <502765299@qq.com>
2020-04-25 04:33:54 +0800

    +增加 移动平台可以带着  人一起走 的功能

---
+++ AirKnight/Assets/_ak/_scripts/Gameplay/actor_common/PlatformCatcher.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformCatcher : MonoBehaviour
+{
+    List<GameObject> attachItems = new List<GameObject>();
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        TryAttach(collision);
+    }
+
+    
+    // private void OnCollisionStay2D(Collision2D collision)
+    // {
+    //     Debug.LogWarning("[Stay2D]");
+    //     TryAttach(collision);
+    // }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        TryDetach(collision);
+    }
+    private void TryAttach(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;
+        if (attachItems.IndexOf(other) < 0)
+        {
+            attachItems.Add(other);
+            other.transform.parent = transform;
+        }
+    }
+
+    private void TryDetach(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;
+        int index = attachItems.IndexOf(other);
+        if (index >= 0)
+        {
+            attachItems.RemoveAt(index);
+            other.transform.parent = null;
+        }
+    }
+
+}
