5959467ff7a509c6912a23657f584b95da91993e
ayy <502765299@qq.com>
2020-04-30 13:44:18 +0800

    +增加  Aries Walk 状态

--- AirKnight/Assets/_ak/_scripts/Gameplay/actors/StateMachine/Aries/AriesStateWalk.cs
+++ AirKnight/Assets/_ak/_scripts/Gameplay/actors/StateMachine/Aries/AriesStateWalk.cs
@@ -1,41 +1,31 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ff
 {
-    public class AriesStateIdle : BaseState<AriesEntity>
+    public class AriesStateWalk : BaseState<AriesEntity>
     {
-        private static AriesStateIdle s_instance = null;
+        private static AriesStateWalk s_instance = null;
 
-        public static AriesStateIdle Instance()
+        public static AriesStateWalk Instance()
         {
-            if(s_instance == null)
+            if (s_instance == null)
             {
-                s_instance = new AriesStateIdle();
+                s_instance = new AriesStateWalk();
             }
             return s_instance;
         }
 
-        public override void OnEnter(AriesEntity entity)
-        {
-            base.OnEnter(entity);
-        }
-
-        public override void OnExit(AriesEntity entity)
-        {
-            base.OnExit(entity);
-        }
-
-        public override void Update(AriesEntity entity,float dt)
+        public override void Update(AriesEntity entity, float dt)
         {
             AriesController ctrl = entity.GetAgent().GetComponent<AriesController>();
             Rigidbody2D rigid = entity.GetAgent().GetComponent<Rigidbody2D>();
             ctrl.UpdateHorizontalMove();
 
-            if (Mathf.Abs(rigid.velocity.x) > 0)
+            if (Mathf.Abs(rigid.velocity.x) == 0)
             {
-                entity.GetFSM().ChangeState(AriesStateWalk.Instance());
+                entity.GetFSM().ChangeState(AriesStateIdle.Instance());
             }
             if (ctrl.m_jumpButton.IsPress())
             {
@@ -46,11 +36,10 @@ namespace ff
         public override void FixedUpdate(AriesEntity entity, float dt)
         {
             EnvironmentDetector envDetector = entity.GetAgent().GetComponent<EnvironmentDetector>();
-            if(!envDetector.isOnGround)
-            {   
+            if (!envDetector.isOnGround)
+            {
                 entity.GetFSM().ChangeState(AriesStateJump1.Instance());
             }
         }
     }
-
 }
