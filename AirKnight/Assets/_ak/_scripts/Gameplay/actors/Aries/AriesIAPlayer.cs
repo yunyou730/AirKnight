cb19e2ebbb4e11693976f0d49a80b17a97d68550
ayy730 <502765299@qq.com>
2020-05-05 20:58:09 +0800

    +新增 通用开关,以及被通用开关打开的门

---
+++ AirKnight/Assets/_ak/_scripts/Gameplay/actors/Aries/AriesIAPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ff
+{
+    public class AriesIAPlayer : IAPlayer
+    {
+        AriesController m_ctrl = null;
+        void Awake()
+        {
+            m_ctrl = GetComponent<AriesController>();
+        }
+
+        public override bool CheckIAButon()
+        {
+            return m_ctrl.m_interactButton.IsPress();
+        }
+    }
+
+}
---
+++ AirKnight/Assets/_ak/_scripts/Gameplay/actors/Interactive/IAPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ff
+{
+    public abstract class IAPlayer : MonoBehaviour
+    {
+        public abstract bool CheckIAButon();
+    }
+}
